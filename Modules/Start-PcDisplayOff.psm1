90b6b6e1c4a12228a091f7227deb1df3cfc0e4c9
www-tacos-asrock <www.0923.tacos@gmail.com>
2021-08-02 00:51:06 +0900

    add: PCの電源関連のスクリプトを追加

---
+++ Modules/Start-PcDisplayOff.psm1
@@ -0,0 +1,57 @@
+function Start-PcDisplayOff {
+  <#
+  .SYNOPSIS
+    PCのディスプレイをオフにする
+
+  .DESCRIPTION
+    PCのディスプレイ表示のみをオフにする
+    コマンド実行して2秒後に画面をオフにする
+    スリープにはならず実行中のプロセスはそのまま実行される
+
+  .PARAMETER Help|h
+    ヘルプを表示する
+
+  .PARAMETER CommonParameters
+    サポートしていません
+
+  .INPUTS
+    なし
+
+  .OUTPUTS
+    なし
+
+  .EXAMPLE
+    PS> Start-PcDisplayOff
+      PCのディスプレイをオフにする
+
+  .EXAMPLE
+    PS> Start-PcDisplayOff -Help
+      ヘルプを表示する
+
+  .LINK
+    参考: Qiita記事「PowerShellメモ　シャットダウン、再起動、スリープ」
+    https://qiita.com/Kosen-amai/items/4b773c077a588f2a2fb5
+  #>
+  Param(
+    [Alias('h')][switch] $Help
+  )
+  if($Help){ Get-Help Start-PcDisplayOff; return }
+
+  #------------------------------
+  # Main処理
+  #------------------------------
+  Add-Type -AssemblyName System.Windows.Forms
+  $form = New-Object System.Windows.Forms.Form
+  $message = [System.Windows.Forms.Message]::Create($form.Handle, 274, 61808, 2)
+  $nativeWindow = New-Object System.Windows.Forms.NativeWindow
+
+  Start-Sleep -Seconds 2
+
+  $nativeWindow.DefWndProc([ref]$message)
+}
+# 関数をそのままExportする場合
+Export-ModuleMember -Function Start-PcDisplayOff
+
+# Aliasも設定してExportする場合
+# Set-Alias <alias> Start-PcDisplayOff
+# Export-ModuleMember -Function Start-PcDisplayOff -Alias <alias>
---
+++ Modules/Start-PcSleep.psm1
@@ -0,0 +1,52 @@
+function Start-PcSleep {
+  <#
+  .SYNOPSIS
+    PCをスリープ状態にする
+
+  .DESCRIPTION
+    PCをスリープ状態にする
+
+  .PARAMETER Help|h
+    ヘルプを表示する
+
+  .PARAMETER CommonParameters
+    サポートしていません
+
+  .INPUTS
+    なし
+
+  .OUTPUTS
+    なし
+
+  .EXAMPLE
+    PS> Start-PcSleep
+      PCをスリープさせる
+
+  .EXAMPLE
+    PS> Start-PcSleep -Help
+      ヘルプを表示する
+
+  .LINK
+    参考: Qiita記事「PowerShellメモ　シャットダウン、再起動、スリープ」
+    https://qiita.com/Kosen-amai/items/4b773c077a588f2a2fb5
+  #>
+  Param(
+    [Alias('h')][switch] $Help
+  )
+  if($Help){ Get-Help Start-PcSleep; return }
+
+  #------------------------------
+  # Main処理
+  #------------------------------
+  Add-Type -AssemblyName System.Windows.Forms
+  $state = [System.Windows.Forms.PowerState]::Suspend
+  $force = $true
+  $disableWakeEvent = $false
+  [System.Windows.Forms.Application]::SetSuspendState($state, $force, $disableWakeEvent)
+}
+# 関数をそのままExportする場合
+Export-ModuleMember -Function Start-PcSleep
+
+# Aliasも設定してExportする場合
+# Set-Alias <alias> Start-PcSleep
+# Export-ModuleMember -Function Start-PcSleep -Alias <alias>
