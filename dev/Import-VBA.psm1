efe6aca148890518992737af6d7b6c38fccb0187
www-tacos-asrock <www.0923.tacos@gmail.com>
2024-04-01 14:24:23 +0900

    新規コマンドレットの作成（開発途中）
    * VBAの管理
    * YesNoダイアログの制御

---
+++ dev/Import-VBA.psm1
@@ -0,0 +1,53 @@
+﻿function Import-VBA {
+  <#
+  .SYNOPSIS
+    <コマンドの概要>
+
+  .DESCRIPTION
+    <コマンドの説明>
+
+  .PARAMETER Help|h
+    ヘルプを表示する
+
+  .PARAMETER <PARAM>
+    <パラメータの説明>
+
+  .PARAMETER CommonParameters
+    サポートしていません
+
+  .INPUTS
+    <入力の概要>
+
+  .OUTPUTS
+    <出力の概要>
+
+  .EXAMPLE
+    PS> Import-VBA
+      <コマンドのみ実行時の処理内容>
+
+  .EXAMPLE
+    PS> Import-VBA -Help
+      ヘルプを表示する
+
+  .EXAMPLE
+    PS> Import-VBA <PARAM>
+      <パラメータを指定したときの処理内容>
+
+  .LINK
+    参考: <参考サイト概要>
+    <URL>
+  #>
+  Param(
+    [Alias('h')][switch] $Help
+  )
+  if ($Help) { Get-Help Import-VBA; return }
+
+  #------------------------------
+  # Main処理
+  #------------------------------
+  # Open(fullname)
+  # book.VBProject.VBComponents.Import(fullname)
+  # book.save()
+}
+# コマンドレットをExport
+Export-ModuleMember -Function Import-VBA
---
+++ dev/Read-YesNo.psm1
@@ -0,0 +1,61 @@
+﻿function Read-YesNo {
+  <#
+  .SYNOPSIS
+     コンソール上でYesNoダイアログを表示するコマンドレット
+
+  .DESCRIPTION
+    コンソール上に[yY/nN]をメッセージ付きで表示する
+    入力を受け取りyならTrueを、nならFalseを返す
+
+  .PARAMETER Help|h
+    ヘルプを表示する
+
+  .PARAMETER Message
+    YesNoを確認する前のメッセージ
+
+  .PARAMETER CommonParameters
+    サポートしていません
+
+  .INPUTS
+    文字列
+
+  .OUTPUTS
+    真偽値
+
+  .EXAMPLE
+    PS> Read-YesNo
+      コンソールに「Yes or No [yY/nN]」を表示し入力を受け付ける
+
+  .EXAMPLE
+    PS> Read-YesNo -Help
+      ヘルプを表示する
+
+  .EXAMPLE
+    PS> Read-YesNo -Message "対応しますか？"
+      コンソールに「対応しますか？ [yY/nN]」を表示し入力を受け付ける
+  #>
+  Param(
+    [Alias('h')][switch] $Help,
+    [string] $Message = "Yes or No"
+  )
+  if ($Help) { Get-Help Read-YesNo; return }
+
+  #------------------------------
+  # Main処理
+  #------------------------------
+  Write-Host "${Message} [yY/nN]"
+  while ($True) {
+    Write-Host ">> " -NoNewLine
+    $yn = Read-Host
+    if ($yn -match "^[yYnN]$") {
+      break
+    }
+    else {
+      Write-Host "無効な入力です [${yn}]`n"
+      continue
+    }
+  }
+  return ($yn -match "^[yY]$")
+}
+# コマンドレットをExport
+Export-ModuleMember -Function Read-YesNo
